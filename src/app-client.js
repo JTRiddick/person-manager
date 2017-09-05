3518f4a2fd4be917ac70ac389a1566d0464907f9
Tatrakrad <riddick.taylor@gmail.com>
2017-09-05 10:39:43 -0500

    redux added

--- src/app-client.js
+++ src/app-client.js
@@ -4,10 +4,21 @@ import React from 'react';
 import ReactDOM,{render} from 'react-dom';
 import { BrowserRouter as Router, browserHistory } from 'react-router-dom';
 import { Routes } from './components/Routes';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 
+import rootReducer from './reducers/index';
+
+let store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(/* middleware,middleware */)
+));
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Routes />
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Routes />
+    </Router>
+  </Provider>
 , document.getElementById('main'));
---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+
+
+const initialState = {
+  // initial state object
+};
+
+
+const rootReducer = combineReducers({
+  state: (state = {}) => state
+
+});
+
+export default rootReducer;
