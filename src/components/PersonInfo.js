ab46ce79a26760cfade1b574caea44e731ad55ce
Tatrakrad <riddick.taylor@gmail.com>
2017-09-05 14:07:04 -0500

    basic table display

--- src/components/PersonInfo.js
+++ src/components/PersonInfo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {csv} from 'd3-request';
+import _ from 'lodash';
 
 import style from '../sass/style.scss';
 import data from '../public/data/Employees.csv';
@@ -11,17 +12,49 @@ export default class PersonInfo extends Component {
     this.state = {};
   }
 
+  mapPeople(data){
+    this.setState({
+      people: data.map(person,i,arr => ({
+        FirstName:person['First Name'],
+        LastName:person['Last Name'],
+      }))
+    })
+  }
+
+  loadList(data){
+    if (data === {}) {
+    return (<div className={`container ${style.denied}`}>
+      <Panel header="There's a Problem!" bsStyle="danger">
+        <p>No People Found</p>
+      </Panel>
+  </div>)}else{
+    //test
+
+    return _.map(data, person => {
+      return(
+        <li className="list-group-item" key={person['ID']}>
+          <div>
+            <h5>
+              {person['First Name']}
+            </h5>
+            <h5>
+              <b>{person['Last Name']}</b>
+            </h5>
+          </div>
+        </li>
+      )
+    })
+  }
+  }
+
   componentWillMount(){
     console.log('employees ...',data);
     if (!data){
       this.setState({loadError:true})
       console.log('csv load error');
     }else{
-      this.setState({
-        people: data.map(person => ({
-          FirstName:person['First Name'],LastName:person['Last Name']
-        }))
-      })
+      const people = _.mapKeys(data,'ID');
+      this.setState({people: {...people}});
     }
   }
 
@@ -31,7 +64,9 @@ export default class PersonInfo extends Component {
       <div id={style.CSVload}>
         <h1>Hi, I'm working now</h1>
         <div>
-      
+          <ul>
+            {this.loadList(this.state.people)}
+          </ul>
         </div>
       </div>
     );
